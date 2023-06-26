c5a9cda9011905dda31bd6bee67f90c928ff2569
Yuma.Kurihara <yuma.kurihara@aijus.com>
2023-06-26 16:23:48 +0900

    ・Realmの導入 ・フォルダの作成

---
+++ TaskTool/View/HomeView/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  TaskTool
+//
+//  Created by aijus on 2023/06/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
