475142bfd57275e7a17a1a8b6de52094dc43c8b8
Yuma.Kurihara <yuma.kurihara@aijus.com>
2023-06-26 15:24:19 +0900

    Initial Commit

---
+++ TaskTool/TaskToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskToolApp.swift
+//  TaskTool
+//
+//  Created by aijus on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
