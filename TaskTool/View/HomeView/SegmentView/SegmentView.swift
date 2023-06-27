360daf61b4b6255d5bde64d218347a42ff9c8315
Yuma.Kurihara <yuma.kurihara@aijus.com>
2023-06-27 14:59:59 +0900

    SegmentView　UI完成

--- TaskTool/View/HomeView/SegmentView/SegmentView.swift
+++ TaskTool/View/HomeView/SegmentView/SegmentView.swift
@@ -8,39 +8,11 @@
 import SwiftUI
 
 struct SegmentView: View {
-    @State var SegmentOrder: [String] = ["日付順", "重要度順"]
-    @State var SelectOrder: [String] = ["日付順"]
     var body: some View {
         VStack{
             TitleView()
             StatusView(viewModel: HomeViewModel(model: HomeModel()))
-        }
-    }
-}
-struct StatusView: View {
-    @StateObject var viewModel: HomeViewModel
-    @State var statusList: [String] = ["全て","未完了","完了"]
-    var body: some View {
-        HStack{
-            ForEach(0..<statusList.count) { num in
-                Button {
-                    viewModel.changeStarsLength(num)
-                } label: {
-                    viewModel.status[num] ?
-                    Text(statusList[num])
-                        .foregroundColor(.white)
-                        .frame(width: 90, height: 33)
-                        .background(Color(red: 0.24, green: 0.24, blue: 0.24))
-                        .cornerRadius(100)
-                    :
-                    Text(statusList[num])
-                        .foregroundColor(Color(red: 0.24, green: 0.24, blue: 0.24))
-                        .frame(width: 90, height: 33)
-                        .background(Color.white)
-                        .cornerRadius(100)
-                }
-                .animation(.easeInOut, value: viewModel.status)
-            }
+            SegmentSetView()
         }
     }
 }
@@ -77,3 +49,60 @@ struct TitleView: View {
         }
     }
 }
+
+struct StatusView: View {
+    @StateObject var viewModel: HomeViewModel
+    @State var statusList: [String] = ["全て","未完了","完了"]
+    var body: some View {
+        HStack{
+            ForEach(0..<statusList.count, id: \.self) { num in
+                Button {
+                    viewModel.changeStarsLength(num)
+                } label: {
+                    viewModel.status[num] ?
+                    Text(statusList[num])
+                        .foregroundColor(.white)
+                        .frame(width: 100, height: 33)
+                        .background(Color(red: 0.24, green: 0.24, blue: 0.24))
+                        .cornerRadius(100)
+                    :
+                    Text(statusList[num])
+                        .foregroundColor(Color(red: 0.24, green: 0.24, blue: 0.24))
+                        .frame(width: 100, height: 33)
+                        .background(Color.white)
+                        .cornerRadius(100)
+                }
+                .animation(.easeInOut, value: viewModel.status)
+            }
+        }
+    }
+}
+
+
+struct SegmentSetView: View {
+    @State var SegmentOrder: [String] = ["日付順", "重要度順"]
+    @State var SelectOrder: String = "日付順"
+    init() {
+        UISegmentedControl.appearance().setTitleTextAttributes(
+            [.foregroundColor : UIColor.white], for: .normal
+        )
+        
+        UISegmentedControl.appearance().setTitleTextAttributes(
+            [.foregroundColor : UIColor.black], for: .selected
+        )
+    }
+    var body: some View {
+        Picker(selection: $SelectOrder, label: Text("")) {
+            ForEach(0..<self.SegmentOrder.count, id: \.self) { id in
+                Text(SegmentOrder[id]).tag(0).tag(self.SegmentOrder[id])
+            }
+        }
+        .background(Color("MainColor"))
+        .pickerStyle(SegmentedPickerStyle())
+        .cornerRadius(10)
+        .clipped()
+        .pickerStyle(.segmented)
+        .frame(width: UIScreen.main.bounds.width / 1.2)
+    }
+}
+
