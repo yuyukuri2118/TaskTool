2c4b2b377d4ee9050f8d158de5eb607bfb75c3a2
Yuma.Kurihara <yuma.kurihara@aijus.com>
2023-06-27 15:30:51 +0900

    TaskViewの作成開始

---
+++ TaskTool/View/HomeView/TaskView/TaskView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskView.swift
+//  TaskTool
+//
+//  Created by aijus on 2023/06/27.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskView()
+    }
+}
