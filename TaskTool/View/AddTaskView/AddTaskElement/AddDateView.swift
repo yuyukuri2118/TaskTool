690aa480175158f1a39820f0b1a37037d5fa7e18
yuyukuri2118 <kurikuri1225yu@gmail.com>
2023-12-20 16:35:25 +0900

    並び替え&絞り込み

--- TaskTool/View/AddTaskView/AddTaskElement/AddDateView.swift
+++ TaskTool/View/AddTaskView/AddTaskElement/AddDateView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AddDateView: View {
     @State var selectedDate:Date = Date()
     @State private var sheet = false
-    @Binding var AddDate: String
+    @Binding var AddDate: Date
     @Binding var AddTime: String
     var body: some View {
         VStack {
@@ -19,21 +19,22 @@ struct AddDateView: View {
                     .environment(\.locale, Locale(identifier: "ja_JP"))
                     .offset(x: -UIScreen.main.bounds.width / 6)
                     .onChange(of: selectedDate) {
-                        formatDate(selectedDate)
+                        AddDate = selectedDate
+                        //formatDate(selectedDate)
                     }
             }.frame(width: UIScreen.main.bounds.width / 1.26)
         }
     }
-    private func formatDate(_ date: Date) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy/MM/dd"
-        let dateString = dateFormatter.string(from: date)
-        
-        let timeFormatter = DateFormatter()
-        timeFormatter.dateFormat = "HH:mm"
-        let timeString = timeFormatter.string(from: date)
-        
-        AddDate = "\(dateString) "
-        AddTime = "\(timeString)"
-    }
+//    private func formatDate(_ date: Date) {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyyy/MM/dd"
+//        let dateString = dateFormatter.string(from: date)
+//        
+//        let timeFormatter = DateFormatter()
+//        timeFormatter.dateFormat = "HH:mm"
+//        let timeString = timeFormatter.string(from: date)
+//        
+//        AddDate = "\(dateString) "
+//        AddTime = "\(timeString)"
+//    }
 }
